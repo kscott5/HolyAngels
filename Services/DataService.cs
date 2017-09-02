e77bd19868782d93de2e03e47f5dc7e4940481a4
Karega Scott <kkscott134@gmail.com>
2017-09-01 21:31:53 -0500

    Initial Stub Data Store Access

---
+++ Services/DataService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using MongoDB.Driver;
+
+namespace HolyAngels.Services {
+    public class DataService {
+        
+    }
+}
