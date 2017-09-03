477796a8887ad601c3f4edcd1a834a5a91998f52
Karega Scott <kkscott134@gmail.com>
2017-09-03 12:07:25 -0500

    Renamed

---
+++ Models/PageModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace HolyAngels.Models
+{
+    public class PageModel
+    {
+        public PageModel() : this("Page"){}
+
+        public PageModel(string pageTitle)
+        {
+            SubTitle = "Holy Angel Home";
+            PageTitle = pageTitle;
+            SubTitle = pageTitle;
+            PageName = pageTitle;
+            MetaKeywords = "Holy Angel Church Men Coalition";
+            MetaDescription = "Holy Angel Home Page for the Church";
+            MetaSubject = "Holy Angel Church";
+            AccessSettings = new List<string>();
+        }
+        
+        public string MetaKeywords { get; set; }
+        public string MetaDescription { get; set; }
+        public string MetaSubject { get; set; }
+        public string PageTitle { get; set; }
+        public string SubTitle { get; set; }        
+        public string PageName {get; set; }
+        public IList<string> AccessSettings {get; set;}
+    }
+}
