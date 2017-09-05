d51ff43f27aa7ed7352f43712f480f2e8b566517
Karega K. Scott <kkscott@outlook.com>
2017-09-05 14:53:31 -0500

    Included semicolon

--- src/Models/MinistryModel.cs
+++ src/Models/MinistryModel.cs
@@ -10,7 +10,7 @@ namespace HolyAngels.Models
         public MinistryModel(){
         }
 
-        public int Id {get; set};
+        public int Id {get; set;}
 
         [Display(Name = "Description")]
         [DataType(DataType.Text)]
