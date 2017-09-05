dcab42af533e6cbf8db61fb7e0b0b7d1b77551ae
Karega K. Scott <kkscott@outlook.com>
2017-09-05 15:46:02 -0500

    Trying w/o compiling or intellisense locally

--- src/Models/CategoryModel.cs
+++ src/Models/CategoryModel.cs
@@ -5,23 +5,15 @@ using System.Collections.Generic;
 
 namespace HolyAngels.Models
 {
-    public abstract class CategoryModel
+    public sealed class CategoryModel
     {
-        public int Id {get; set;}
-        
-        public abstract int Type {get; private set;}
-
+        public int Id { get; set; }        
         public string Name { get; set; }
         public string Description { get; set; }
     }
 
     public class MinistryCategoryModel : CategoryModel
     {
-        public MinistryCategoryModel()
-        {
-            this.Type = 1;
-        }
-    
-        public override int Type {get; private set;}
+        public MinistryCategoryModel() {} 
     }
 }
