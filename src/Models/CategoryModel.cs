3d7faf0b21cfc834688cebae7887196cdf787f5c
Karega K. Scott <kkscott@outlook.com>
2017-09-05 15:50:29 -0500

    Again

--- src/Models/CategoryModel.cs
+++ src/Models/CategoryModel.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace HolyAngels.Models
 {
-    public sealed class CategoryModel
+    public class CategoryModel
     {
         public int Id { get; set; }        
         public string Name { get; set; }
