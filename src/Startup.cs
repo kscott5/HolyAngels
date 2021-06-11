30c69bf661c2293bc8c2bbd356c572cd89c37dec
Karega K. Scott <kkscott@outlook.com>
2021-06-11 12:29:09 -0500

    IWebHostEnvironment spelling error.

--- src/Startup.cs
+++ src/Startup.cs
@@ -39,7 +39,7 @@ namespace HolyAngels
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
