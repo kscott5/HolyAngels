e89c96f9b8172ca4d8b36e2ae8f2b5f38029a033
Karega K. Scott <kkscott@outlook.com>
2021-06-11 12:45:37 -0500

    Import namespace with IHostEnvironment extension methods.

--- src/Startup.cs
+++ src/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
