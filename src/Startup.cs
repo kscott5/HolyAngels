0755c0a40ed709d366b02583aade52db64e6e08d
Karega K. Scott <kkscott@outlook.com>
2021-06-11 12:17:04 -0500

    IHostingEnvironment is obsolete.

--- src/Startup.cs
+++ src/Startup.cs
@@ -39,7 +39,7 @@ namespace HolyAngels
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
