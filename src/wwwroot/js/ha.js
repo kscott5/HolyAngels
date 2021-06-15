ad9856f36e76e3d806de5f35bfe397da58791db0
Karega K. Scott <kkscott@outlook.com>
2021-06-15 13:20:50 -0500

    Fullcalendar 4x migration from 3x.

--- src/wwwroot/js/ha.js
+++ src/wwwroot/js/ha.js
@@ -1,105 +1,39 @@
-﻿var HA = (function () {
-    var HA = function () {
-        return new Object();
-    };
-});
+﻿function HolyAngels() {
+	if(!this instanceof HolyAngels) {
+		return new HolyAngels();
+	}
 
-$(document).ready(function () {
-    HA.HtmlForm = window.document.forms[0];
+    this.HtmlForm = window.document.forms[0];
 
-    HA.author = "Karega Scott";
-    HA.Version = "1.0.0";
-    HA.UnsecuredHostBaseUrl = "http://" + document.location.host;
-    HA.SecuredHostBaseUrl = "https://" + document.location.host;
-    HA.ActualHostBaseUrl = document.location.protocol + "//" + document.location.host;
-});
+    this.author = "Karega Scott";
+    this.Version = "1.0.0";
+    this.UnsecuredHostBaseUrl = "http://" + document.location.host;
+    this.SecuredHostBaseUrl = "https://" + document.location.host;
+    this.ActualHostBaseUrl = document.location.protocol + "//" + document.location.host;
 
-function _showDialog (title, data) {
-    $("#dialog").html(data);
-
-    $("#dialog").dialog({
-        title: title,
-        model: true
-    });
+	return this;
 }
 
+HolyAngels.prototype.initEventCalendar = () => {
+	var objThis = this;
+	
+	var el = document.querySelector('#calendar');
+	var calendar = new FullCalendar.Calendar(el, {
+		plugins: ['dayGrid'],
+		defaultView: 'dayGridMonth'
+    	header: {
+        	left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        editable: false,
+      }); // fullcalendar
 
-HA.eventCalendar = HA.prototype = {
-    init: function () {
-        $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            editable: false,
-            events: function (start, end, callback) {
-                var tokenSet = 'undefined';
-                $.each(document.cookie.split(';'), function(index,item){
-                    var pair = item.split('=');
-                    if(pair[0] == 'X-XSRF-TOKEN') {
-                        tokenSet = pair;
-                        return;
-                    }
-                });
-                
-                $.ajax({
-                    url: HA.ActualHostBaseUrl + '/eventcalendar/events',
-                    type: 'POST',
-                    data: {date: start},
-                    headers: {"X-XSRF-TOKEN": tokenSet[1]},
-                    success: function (results) {
-                        var events = [];
-                        results.forEach(function (result) {
-                            // Shouldn't haven't to call replace twice...
-                            var resultStart = result.TimeStamps.StartDate;
-                            //var resultEnd = result.TimeStamps.EndDate;
+	calendar.render();
+ }; // initEventCalendar
 
-                            var url = "javascript:HA.eventCalendar.showDialog({" +
-                                "Title: '" + result.Title + "'," +
-                                "Location: '" + result.Location + "'," +
-                                "Description: '" + result.Description + "'," +
-                                "Speakers: '" + result.Speakers + "'," +
-                                "Start: '" + resultStart + "'," +
-                               // "End: '" + resultEnd + "'," +
-                                "})";
+var HA = {};
+document.addEventListener('DOMContentLoaded', () => {
+	HA = new HolyAngels();
+});
 
-
-                            events.push({
-                                title: result.Title,
-                                start: resultStart,
-                               // end: resultEnd,
-                                url: url,
-                                allDay: false
-                            });
-                        });
-                        callback(events);
-                    },
-                    error: function () { }
-                });
-            }
-        });
-    },
-
-    showDialog: function (options) {
-        var start = new Date(options.Start);
-        var end = new Date(options.End);
-
-        var dates = $.fullCalendar.formatDate(start, "MMMM dd") + "&nbsp;to&nbsp;" + 
-                    $.fullCalendar.formatDate(end, "MMMM dd, yyyy")
-    
-        var times = $.fullCalendar.formatDate(start, "HH:mm TT") + "&nbsp;to&nbsp;" + 
-                    $.fullCalendar.formatDate(end, "HH:mm TT")
-    
-        var html =
-        "<table>" +
-        "<tr><td>Date:</td><td>" + dates + "</td></tr>" +
-        "<tr><td>Time:</td><td>" + times + "</td></tr>" +
-        "<tr><td>Speakers:</td><td>" + options.Speakers + "</td></tr>" +
-        "<tr><td>Location:</td><td>" + options.Location + "</td></tr>" +
-        "<tr><td colspan='2'>" + options.Description + "</td></tr>" +
-        "</table>";
-
-        _showDialog(options.Title, html);
-    },
-};
